af3867a0753c9689aa7c54ce97c1fd452265f68a
Bryce <boylebryce@gmail.com>
2020-01-04 14:47:19 -0800

    change include path

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
     define('WEB_ROOT', $_SERVER['DOCUMENT_ROOT']);
-    define('INCLUDE_DIR', dirname(WEB_ROOT));
+    define('INCLUDE_DIR', dirname(WEB_ROOT) . '/include/booru-api');
     define('CONFIG_DIR', INCLUDE_DIR . '/config');
 
     echo WEB_ROOT . '<br>';
