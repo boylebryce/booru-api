15c5bd9c052f46dda85060af4d93916a99ad0d80
Bryce <boylebryce@gmail.com>
2020-01-04 17:55:26 -0800

    initial commit

---
+++ add_tags.php
@@ -0,0 +1,9 @@
+<?php
+
+    // contains paths to resources relative to web root
+    require_once 'paths.php';
+
+    // contains various settings, such as database information
+    require_once CONFIG_DIR . '/config.php';
+
+?>
